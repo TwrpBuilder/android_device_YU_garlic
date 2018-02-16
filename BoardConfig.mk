fceee1627c602db2c05cded0e9895e3ca3292b26
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-02-16 19:30:06 +0200

    Build kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,10 +26,26 @@ BOARD_FLASH_BLOCK_SIZE := 1000000
 
 
 # Kernel
-TARGET_PREBUILT_KERNEL := device/YU/garlic/kernel
-BOARD_KERNEL_CMDLINE := g_android.luns=1 g_android.removable=1 g_android.cdrom=1 console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 $
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864 
+BOARD_FLASH_BLOCK_SIZE := 1000000
+
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 $
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_CONFIG := lineageos_garlic_defconfig
+TARGET_KERNEL_SOURCE := kernel/yu/msm8937
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
 
 include device/generic/twrpbuilder/BoardConfig64.mk
